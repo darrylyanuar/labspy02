396611d17434198092e9bbb2cd456071aecbe8ef
darryl <darryl>
2022-11-09 21:36:37 +0700

    file python

---
+++ praktikum2.py
@@ -0,0 +1,11 @@
+
+a = int(input("Masukkan bilangan A: "))
+b = int(input("Masukkan bilangan B: "))
+c = int(input("Masukkan bilangan C: "))
+
+if a > b and a > c:
+  print('A yang terbesar')
+elif b > a and b > c:
+  print('B yang terbesar')
+else:
+  print('C yang terbesar')
